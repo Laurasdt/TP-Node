c52a9d668f87985a68f4a6db2887d0ff7998d1c9
Laura Sdt <lauramarie.sdt@gmail.com>
2025-01-09 18:58:53 +0100

    express set up

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ require("dotenv").config();
 
 const { PORT, HOSTNAME } = process.env;
 
-const students = [
-  { name: "Sonia", birth: "2019-14-05" },
-  { name: "Antoine", birth: "2000-12-05" },
-  { name: "Alice", birth: "1990-14-09" },
-  { name: "Sophie", birth: "2001-10-02" },
-  { name: "Bernard", birth: "1980-21-08" },
-];
+const bodyParser = require("body-parser");
 
-server.listen(PORT || 8000, HOSTNAME, () => {
-  console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("assets"));
+
+app.set("view engine", "pug");
+app.set("views", "./view");
+
+app.get("/", (req, res) => {
+  res.send("Le serveur Express fonctionne !");
 });
+
+app.listen(PORT || 8000, HOSTNAME || "127.0.0.1", () => {
+  console.log(
+    `Serveur en cours d'exécution à l'adresse : http://${
+      HOSTNAME || "127.0.0.1"
+    }:${PORT || 8000}`
+  );
+});
+node;
